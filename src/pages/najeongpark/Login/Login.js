55b310f5002a3284475cb8c3b3c7815b5d6877bc
ppnj0213 <ppnj0213@gmail.com>
2022-11-24 16:51:57 +0900

    Login page 추가

--- src/pages/najeongpark/Login/Login.js
+++ src/pages/najeongpark/Login/Login.js
@@ -1,12 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../Login/Login.scss';
 
+// import '../../../styles/mixin.scss';
+
 const Login = () => {
+    const [id, setId] = useState('');
+    const [password, setPassword] = useState('');
+    const [isActive, setIsActive] = useState(true);
+    const [isActiveBtn, setIsActiveBtn] = useState(true);
+    const navigate = useNavigate();
+    //로그인 버튼 활성화
+    const valueChk = () => {
+        id && password ? setIsActive(false) : setIsActive(true);
+    };
+
     return (
-        <div>
-            <h1>나정이의 로그인페이지</h1>
-        </div>
+        <header className="Login">
+            <h1>westagram</h1>
+            <main className="userInput">
+                <input
+                    onChange={e => {
+                        setId(e.target.value);
+                    }}
+                    onKeyUp={valueChk}
+                    className="id"
+                    type="text"
+                    placeholder="전화번호, 사용자 이름 또는 이메일"
+                />
+                <input
+                    onChange={e => {
+                        setPassword(e.target.value);
+                    }}
+                    onKeyUp={valueChk}
+                    className="pass"
+                    type="password"
+                    placeholder="비밀번호"
+                />
+                <button
+                    className={isActive ? 'loginUnActive' : 'loginActive'}
+                    // onClick={validationChk}
+                >
+                    로그인
+                </button>
+                {!isActive && !isActiveBtn && (
+                    <p className="errorMsg">로그인 정보를 확인해주세요.</p>
+                )}
+            </main>
+            <div className="footer">
+                <span>비밀번호를 잊으셨나요?</span>
+            </div>
+        </header>
     );
 };
 
