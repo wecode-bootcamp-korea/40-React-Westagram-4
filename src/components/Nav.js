c09c57f8f225740e8f1866c1955ea8c4f07bcbd5
김채영 <cccodud@gmail.com>
2022-11-25 11:43:52 +0900

    nav bar  수정사항 반영

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,40 +1,38 @@
 import React from 'react';
-import '../components/Nav.scss';
+import './Nav.scss';
 
 const Nav = () => {
     return (
-        <div>
-            <nav>
-                <div className="navContainer">
-                    <div className="navLogo">
-                        <img
-                            className="logoImg"
-                            src="/images/nav/logo.png"
-                            alt="로고"
-                        />
-                        <div className="vl" />
-                        <span className="logoBrand">Westagram</span>
-                    </div>
-                    <div className="navSearchBox">
-                        <input id="search" type="text" placeholder="검색" />
-                    </div>
-                    <div className="navIcon">
-                        <img
-                            src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/explore.png"
-                            alt="탐색"
-                        />
-                        <img
-                            src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png"
-                            alt="하트"
-                        />
-                        <img
-                            src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/profile.png"
-                            alt="마이페이지"
-                        />
-                    </div>
+        <nav className="nav">
+            <div className="navContainer">
+                <div className="navLogo">
+                    <img
+                        className="logoImg"
+                        src="/images/nav/logo.png"
+                        alt="로고"
+                    />
+                    <div className="vl" />
+                    <span className="logoBrand">Westagram</span>
                 </div>
-            </nav>
-        </div>
+                <div className="navSearchBox">
+                    <input id="search" type="text" placeholder="검색" />
+                </div>
+                <div className="navIcon">
+                    <img
+                        src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/explore.png"
+                        alt="탐색"
+                    />
+                    <img
+                        src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png"
+                        alt="하트"
+                    />
+                    <img
+                        src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/profile.png"
+                        alt="마이페이지"
+                    />
+                </div>
+            </div>
+        </nav>
     );
 };
 
