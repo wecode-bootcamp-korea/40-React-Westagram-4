b2597c5a447ff46aa2a9741ae4be057819eac991
ppnj0213 <ppnj0213@gmail.com>
2022-11-29 23:36:01 +0900

    Aside에서 상수데이터 활용

--- src/pages/najeongpark/Main/Main.js
+++ src/pages/najeongpark/Main/Main.js
@@ -225,6 +225,11 @@ const Main = () => {
                                 </div>
                             </div>
                         </div>
+                        <div className="asideBottom">
+                            {ASIDE_FOOTER.map(info => {
+                                return <span key={info.id}>{info.text}</span>;
+                            })}
+                        </div>
                     </div>
                 </aside>
             </main>
@@ -233,3 +238,17 @@ const Main = () => {
 };
 
 export default Main;
+
+const ASIDE_FOOTER = [
+    { id: 1, text: '소개 ・' },
+    { id: 2, text: '도움말 ・' },
+    { id: 3, text: '홍보 센터 ・' },
+    { id: 4, text: 'API ・' },
+    { id: 5, text: '채용 정보 ・' },
+    { id: 6, text: '개인정보처리방침 ・' },
+    { id: 7, text: '약관 ・' },
+    { id: 8, text: '위치 ・' },
+    { id: 9, text: '인기 계정 ・' },
+    { id: 10, text: '해시태그 ・' },
+    { id: 11, text: '언어' },
+];
