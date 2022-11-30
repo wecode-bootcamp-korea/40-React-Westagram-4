29ad8507a99285ff73fccabfc36c9c893e01fe8e
mundayeon <mundayeon93@gmail.com>
2022-11-30 19:47:16 +0900

    상수 데이터 활용

--- src/pages/dayeonmun/Main/Main.js
+++ src/pages/dayeonmun/Main/Main.js
@@ -186,15 +186,15 @@ function Main() {
                 </div>
 
                 <div className="copyright">
-                    <span className="copyright1">
-                        소개 · 도움말 · 홍보 센터 · API · 채용 정보 ·
-                    </span>
-                    <span className="copyright2">
-                        개인정보처리방침 · 약관 · 위치 · 언어
-                    </span>
-                    <span className="copyright3">
-                        © 2022 INSTAGRAM FROM META
-                    </span>
+                    <ul>
+                        {COPYRIGHT_LIST.map(copyrightText => {
+                            return (
+                                <li key={copyrightText.id}>
+                                    {copyrightText.text}
+                                </li>
+                            );
+                        })}
+                    </ul>
                 </div>
             </aside>
         </main>
@@ -202,3 +202,16 @@ function Main() {
 }
 
 export default Main;
+
+const COPYRIGHT_LIST = [
+    { id: 1, text: '소개 · ' },
+    { id: 2, text: '도움말 · ' },
+    { id: 3, text: '홍보 센터 · ' },
+    { id: 4, text: 'API · ' },
+    { id: 5, text: '채용 정보 · ' },
+    { id: 6, text: '개인정보처리방침 · ' },
+    { id: 7, text: '약관 · ' },
+    { id: 8, text: '위치 · ' },
+    { id: 9, text: '언어' },
+    { id: 10, text: '© 2022 INSTAGRAM FROM META' },
+];
